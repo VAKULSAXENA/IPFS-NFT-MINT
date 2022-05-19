1bb8af21502de5b5006ec2207f6b1ded58e26a22
vakul <abhisheksaxena714.vs@gmail.com>
2022-05-19 17:10:55 +0530

    deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,7 +2,7 @@
  *@type import('hardhat/config').HardhatUserConfig
  */
 require("dotenv").config();
-require("ethers");
+require("@nomiclabs/hardhat-ethers");
 const API_URL = process.env.API_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
@@ -12,7 +12,7 @@ module.exports = {
     hardhat: {},
     rinkeby:{
     url:'https://speedy-nodes-nyc.moralis.io/5994c1d73829497f1c9e424d/eth/rinkeby',
-    accounts:PRIVATE_KEY
+    accounts:["3aaf25724408ee89faa2d191db1bee758a3a9b3288a6177294f027292fdec3bc"]
      }
   }
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,19 @@
+
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy();
+    console.log("Contract deployed to address:", myNFT.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+
+
+
+    //0x7cFA9a7E9a8bF6a4f287BC83121A42e4b7dd6295
